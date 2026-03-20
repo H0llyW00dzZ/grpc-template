f5805de1d143677dd74d69424ccc309c0b59a1ec
H0llyW00dzZ <h0llyw00dzz@pm.me>
2026-03-20 13:07:27 +0700

    feat(server): add WithListener option for custom net.Listeners (#4)
    - [+] Add helper_test.go with test certificate generation and assertPanics
    - [+] Implement WithListener option in option.go
    - [+] Update server.go to use injected listener in Run method
    - [+] Add extensive tests in server_test.go for TLS/mTLS (valid/invalid certs/CA), keepalive, max msg size/concurrent streams, grpc options, service registration, invalid ports, closed listeners, and all-options integration

---
+++ internal/server/helper_test.go
@@ -0,0 +1,114 @@
+// Copyright (c) 2026 H0llyW00dzZ All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package server_test
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+// generateTestCert creates a self-signed CA and a leaf certificate in dir.
+// Returns paths to (certFile, keyFile, caCertFile).
+func generateTestCert(t *testing.T, dir string) (certFile, keyFile, caCertFile string) {
+	t.Helper()
+
+	// Generate CA key pair.
+	caKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatalf("generate CA key: %v", err)
+	}
+
+	caTemplate := &x509.Certificate{
+		SerialNumber:          big.NewInt(1),
+		Subject:               pkix.Name{CommonName: "Test CA"},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(time.Hour),
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+	}
+
+	caCertDER, err := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &caKey.PublicKey, caKey)
+	if err != nil {
+		t.Fatalf("create CA cert: %v", err)
+	}
+
+	// Write CA cert PEM.
+	caCertFile = filepath.Join(dir, "ca.pem")
+	writePEM(t, caCertFile, "CERTIFICATE", caCertDER)
+
+	// Generate leaf key pair.
+	leafKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatalf("generate leaf key: %v", err)
+	}
+
+	leafTemplate := &x509.Certificate{
+		SerialNumber: big.NewInt(2),
+		Subject:      pkix.Name{CommonName: "localhost"},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(time.Hour),
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		DNSNames:     []string{"localhost"},
+	}
+
+	caCert, err := x509.ParseCertificate(caCertDER)
+	if err != nil {
+		t.Fatalf("parse CA cert: %v", err)
+	}
+
+	leafCertDER, err := x509.CreateCertificate(rand.Reader, leafTemplate, caCert, &leafKey.PublicKey, caKey)
+	if err != nil {
+		t.Fatalf("create leaf cert: %v", err)
+	}
+
+	// Write leaf cert PEM.
+	certFile = filepath.Join(dir, "cert.pem")
+	writePEM(t, certFile, "CERTIFICATE", leafCertDER)
+
+	// Write leaf key PEM.
+	keyFile = filepath.Join(dir, "key.pem")
+	leafKeyDER, err := x509.MarshalECPrivateKey(leafKey)
+	if err != nil {
+		t.Fatalf("marshal leaf key: %v", err)
+	}
+	writePEM(t, keyFile, "EC PRIVATE KEY", leafKeyDER)
+
+	return certFile, keyFile, caCertFile
+}
+
+func writePEM(t *testing.T, path, blockType string, data []byte) {
+	t.Helper()
+	f, err := os.Create(path)
+	if err != nil {
+		t.Fatalf("create %s: %v", path, err)
+	}
+	defer f.Close()
+	if err := pem.Encode(f, &pem.Block{Type: blockType, Bytes: data}); err != nil {
+		t.Fatalf("encode PEM %s: %v", path, err)
+	}
+}
+
+// assertPanics verifies that fn panics.
+func assertPanics(t *testing.T, name string, fn func()) {
+	t.Helper()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("%s: expected panic, did not panic", name)
+		}
+	}()
+	fn()
+}
